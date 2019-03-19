67662f1ed50d7b002eb48c2a2baa0554d1f44ca4
sarn1 <sarn@tyndale.com>
2019-03-19 16:09:58 -0500

    chap 3

--- DemoECommerceApp/DemoECommerceApp/Controllers/ValuesController.cs
+++ DemoECommerceApp/DemoECommerceApp/Controllers/ValuesController.cs
@@ -1,38 +1,44 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 
 namespace DemoECommerceApp.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+    public class ValuesController : Controller
     {
-        // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+        public IEnumerable<string> Get12()
+        {
+            return new string[] { "value1", "value2", "value3" };
+        }
+
+        // GET api/values
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public string Get(int id)
         {
             return "value";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody]string value)
         {
         }
 
---
+++ DemoECommerceApp/DemoECommerceApp/Domain/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoECommerceApp.Domain
+{
+    public class Product
+    {
+        public Product(int id, string name, decimal price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
